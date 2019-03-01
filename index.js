c4d3abb844a972f3515388fb35104bfc295c9033
Futaba Isshiki <futaba_isshiki@outlook.com>
2019-03-01 09:15:28 +0800

    fix: if only ruby_base, return ruby_base

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 hexo.extend.helper.register('ruby', function(ruby_base, ruby_text){
-  if (!ruby_base || !ruby_text) {
-    return '';
+  if (!ruby_text) {
+    return ruby_base;
   }
   return '<ruby><rb>' + ruby_base + '</rb><rp>(</rp><rt>' + ruby_text + '</rt><rp>)</rp></ruby>';
 });
