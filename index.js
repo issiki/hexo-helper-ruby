aec5481ba6053b2400a9b0bd2205ee2b3c3fdb4e
Futaba Isshiki <futaba_isshiki@outlook.com>
2019-03-01 08:54:09 +0800

    fix: code mistake

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 hexo.extend.helper.register('ruby', function(ruby_base, ruby_text){
-  return '<ruby><rb>' + ruby_base + '</rb><rp>(</rp><rt>' + ruby_base + '</rt><rp>)</rp></ruby>';
+  return '<ruby><rb>' + ruby_base + '</rb><rp>(</rp><rt>' + ruby_text + '</rt><rp>)</rp></ruby>';
 });
