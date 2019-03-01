c92efc0888d3d97fc3c6afe302472c51cf6731f2
Futaba Isshiki <futaba_isshiki@outlook.com>
2019-03-01 09:05:05 +0800

    fix: return '' when !ruby_base or !ruby_text is NOT true

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 'use strict';
 hexo.extend.helper.register('ruby', function(ruby_base, ruby_text){
+  if (!ruby_base || !ruby_text) {
+    return '';
+  }
   return '<ruby><rb>' + ruby_base + '</rb><rp>(</rp><rt>' + ruby_text + '</rt><rp>)</rp></ruby>';
 });
